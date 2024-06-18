7bb19a15594b4e1f4c62acd9f2d3a584abe083ca
Vesna Groselj <vgroselj@gmail.com>
2024-06-18 21:53:37 +0200

    WN-1 Use self instead static.

--- src/Plugin/Field/FieldWidget/MultipleFieldsYoastSeoWidget.php
+++ src/Plugin/Field/FieldWidget/MultipleFieldsYoastSeoWidget.php
@@ -25,9 +25,23 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 final class MultipleFieldsYoastSeoWidget extends YoastSeoWidget implements ContainerFactoryPluginInterface {
 
-  private TextFieldFilter $textFieldFilter;
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    FieldDefinitionInterface $field_definition,
+    array $settings,
+    array $third_party_settings,
+    EntityFieldManagerInterface $entity_field_manager,
+    YoastSeoManager $manager,
+    private TextFieldFilter $textFieldFilter
+  ) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings, $entity_field_manager, $manager);
+  }
 
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
     /** @var EntityFieldManagerInterface $entityFieldManager */
     $entityFieldManager = $container->get('entity_field.manager');
 
@@ -37,7 +51,7 @@ final class MultipleFieldsYoastSeoWidget extends YoastSeoWidget implements Conta
     /** @var TextFieldFilter TextFieldFilter */
     $textFieldFilter = $container->get('wn_realtime_seo.text_field_filter');
 
-    return new static(
+    return new self(
       $plugin_id,
       $plugin_definition,
       $configuration['field_definition'],
@@ -47,23 +61,6 @@ final class MultipleFieldsYoastSeoWidget extends YoastSeoWidget implements Conta
       $yoastSeoManager,
       $textFieldFilter
     );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct(
-    $plugin_id,
-    $plugin_definition,
-    FieldDefinitionInterface $field_definition,
-    array $settings,
-    array $third_party_settings,
-    EntityFieldManagerInterface $entity_field_manager,
-    YoastSeoManager $manager,
-    TextFieldFilter $text_field_filter
-  ) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings, $entity_field_manager, $manager);
-    $this->textFieldFilter = $text_field_filter;
   }
 
   /**
