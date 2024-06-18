e8e10449d78554d4fc6f4c83729c0106ac9011a7
Vesna Groselj <vgroselj@gmail.com>
2024-06-18 16:13:28 +0200

    WM-1 Use sprintf + CS fixes.

--- src/TextFieldFilter.php
+++ src/TextFieldFilter.php
@@ -14,28 +14,16 @@ final class TextFieldFilter {
     private EntityFieldManagerInterface $entityFieldManager
   ) {}
 
-  /**
-   * Filters text fields from the given field definitions.
-   *
-   * @param array $field_definitions
-   *   The list of field definitions.
-   *
-   * @return array
-   *   An associative array of field names and their labels.
-   */
-  public function filterTextFields(array $field_definitions, string $parent_entity = '') {
+  public function filterTextFields(array $field_definitions, string $parent_entity = ''): array {
     $text_fields = [];
 
     foreach ($field_definitions as $field_name => $field_definition) {
-      // TODO sprintf.
-      $field_key = $parent_entity ? "$parent_entity::$field_name" : $field_name;
+      $field_key = $parent_entity ? sprintf('%s::%s', $parent_entity, $field_name) : $field_name;
 
       if ($this->isTextField($field_definition)) {
-        // TODO sprintf.
-        $text_fields[$field_key] = $parent_entity ? "$parent_entity: " . $field_definition->getLabel() : $field_name;
+        $text_fields[$field_key] = $parent_entity ? sprintf('%s: %s', $parent_entity, $field_definition->getLabel()) : $field_name;
       }
       elseif ($this->isEntityReferenceToParagraph($field_definition)) {
-        /** @var array $paragraph_field_definitions */
         $paragraph_field_definitions = $this->getParagraphFieldDefinitions($field_definition);
         $nested_text_fields = $this->filterTextFields($paragraph_field_definitions, $field_key);
         $text_fields = array_merge($text_fields, $nested_text_fields);
@@ -45,55 +33,22 @@ final class TextFieldFilter {
     return $text_fields;
   }
 
-  /**
-   * Checks if a field definition is a text field.
-   *
-   * @param FieldDefinitionInterface $field_definition
-   *   The field definition.
-   *
-   * @return bool
-   *   TRUE if the field is a text field, FALSE otherwise.
-   */
   private function isTextField(FieldDefinitionInterface $field_definition): bool {
     return in_array($field_definition->getType(), $this->textFieldTypes());
   }
 
-  /**
-   * Checks if a field definition is an entity reference to a paragraph.
-   *
-   * @param FieldDefinitionInterface $field_definition
-   *   The field definition.
-   *
-   * @return bool
-   *   TRUE if the field is an entity reference to a paragraph, FALSE otherwise.
-   */
   private function isEntityReferenceToParagraph(FieldDefinitionInterface $field_definition): bool {
     return in_array($field_definition->getType(), ['entity_reference', 'entity_reference_revisions']) &&
       $field_definition->getSetting('handler') === 'default:paragraph';
   }
 
-  /**
-   * Retrieves field definitions for the referenced paragraph.
-   *
-   * @param FieldDefinitionInterface $field_definition
-   *   The field definition.
-   *
-   * @return array
-   *   The field definitions for the referenced paragraph.
-   */
   private function getParagraphFieldDefinitions(FieldDefinitionInterface $field_definition): array {
     $target_bundles = $field_definition->getSetting('handler_settings')['target_bundles'];
     // Assuming single target bundle, adjust logic if multiple bundles are needed.
-    $bundle = reset($target_bundles);
+    $bundle = (string) reset($target_bundles);
     return $this->entityFieldManager->getFieldDefinitions('paragraph', $bundle);
   }
 
-  /**
-   * Returns the list of text field types.
-   *
-   * @return array
-   *   An array of text field types.
-   */
   private function textFieldTypes(): array {
     return [
       'text_with_summary',
